b78dbb2b14191f9504ff79d832b79992b22136b9
surbhtyagi10 <surbhiityagi1999@gmail.com>
2021-05-25 14:21:35 +0530

    set

--- FreelancingPortal/settings.py
+++ FreelancingPortal/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
